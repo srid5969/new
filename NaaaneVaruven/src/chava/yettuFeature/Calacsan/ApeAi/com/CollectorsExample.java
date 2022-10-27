d0328b0e8912741a81e7f5ba539eb853d4b9574b
innovix00 <innovix00@emma-whatson>
2022-10-27 15:42:34 +0530

    sdaert

---
+++ NaaaneVaruven/src/chava/yettuFeature/Calacsan/ApeAi/com/CollectorsExample.java
@@ -0,0 +1,26 @@
+package chava.yettuFeature.Calacsan.ApeAi.com;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CollectorsExample {
+	public static void main(String[] args) {
+		List<Product> productsList = new ArrayList<Product>();
+		// Adding Products
+		productsList.add(new Product(1, "HP Laptop", 25000f));
+		productsList.add(new Product(2, "Dell Laptop", 30000f));
+		productsList.add(new Product(3, "Lenevo Laptop", 28000f));
+		productsList.add(new Product(4, "Sony Laptop", 28000f));
+		productsList.add(new Product(5, "Apple Laptop", 90000f));
+//		System.out.println(productsList
+//				.stream()
+//				.map(x -> x.price) // fetching price
+//				.collect(Collectors.toList()); // collecting as list);
+		List<Float> productPriceList = productsList
+				.stream()
+				.map(x -> x.price) // fetching price
+				.collect(Collectors.toList()); // collecting as list
+		System.out.println(productPriceList);
+	}
+}
